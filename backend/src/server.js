509ccd4cdc1f8813e09ae79f5a794a77af4c879b
mathyaku <mathyaku@gmail.com>
2019-11-12 09:00:18 +0200

    uploading aircnc project to github

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://aircnc-react-web:aircnc-react-web@cluster0-tblk3.mongodb.net/aircnc?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(routes);
+
+app.listen(3333);
+
